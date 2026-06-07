dc1b83c4a0c44e9d2d15532534fd3109342b1fa4
Naveen03navee <naveenb.bnbn@gmail.com>
2026-06-07 14:23:02 +0530

    Initial commit for new automated pipeline

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const session = require('express-session');
+const QRCode = require('qrcode');
 
 app.get('/', (req, res) => {
   res.send('🚀 DevSecOps Pipeline is LIVE! Deployed via Jenkins & Minikube.');
